4f3ca4489a7a7a239e70b463e100a413cbe25cff
tyzan18 <santytt31@gmail.com>
2024-10-04 10:42:42 -0500

    Delete portafolio 2 directory

--- portafolio 2/js/sanciones.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.info-button').forEach(button => {
-    button.addEventListener('click', () => {
-        const infoText = button.nextElementSibling;
-        infoText.textContent = button.getAttribute('data-info');
-        infoText.style.display = 'block';
-    });
-});
